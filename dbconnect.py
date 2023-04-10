143253660f633c3aedf82867d5234288195b09db
Ekamruth <113750381+Ekamruth@users.noreply.github.com>
2023-04-10 21:36:08 +0530

    Added comments to the dbconnect file

--- dbconnect.py
+++ dbconnect.py
@@ -1,14 +1,17 @@
 
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text      
 
+#dbcon_string contains the connection string to the database, including the necessary authentication credentials to connect to the database. 
 dbcon_string="mysql+pymysql://peb9zy44l6y7y03zobpd:pscale_pw_6aI0gwsjCau5piw5eHIyZRBiJwh5suBOoE3ckZu4qmE@ap-south.connect.psdb.cloud/naruto?charset=utf8mb4"
 
+#creating a database engine
 engine = create_engine(dbcon_string,connect_args={
         "ssl": {
             "ssl_ca": "/etc/ssl/cert.pem"
         }
     })
 
+#Fetching information about all characters from the characters table in the naruto database.
 def chars_info():
   with engine.connect() as conn:
     result=conn.execute(text("select * from naruto.characters"))
@@ -18,6 +21,7 @@ def chars_info():
     char_dict.append(row._mapping)
   return char_dict
 
+#Fetching information about a specific character from the characters table in the naruto database.
 def char_info(name):
   with engine.connect() as conn:
     s=("select * from naruto.characters where name='%s'" %name)
